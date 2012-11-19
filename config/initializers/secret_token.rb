02b05c88e547ed6f1d609cc51d2f07fc890f60ee
Malte Schmitz <malte@schmitz-sh.de>
2012-11-19 22:31:11 +0100

    Creates Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Celltrack::Application.config.secret_token = '47c22e8ffec194c152d493b791f89db8e41ef7378fa2317a2dd83118c941d4c14de6b3b553bdb159ce5474def6ff152827a4bf2abf555be18178e97663c69217'
