e556d5a2df64345ea9f262db77bae274f82c7839
Eike von Tils <eike.vontils@informatik.uni-luebeck.de>
2012-12-14 11:25:00 +0100

    Adds counting of cells to lib/tasks/count.rb
    refdataA has
    - 46.246.907 coordinates
    - 12.243.993 border-coordinates and
    -    244.789 cells (sum of all masks)

--- lib/tasks/count.rb
+++ lib/tasks/count.rb
@@ -17,14 +17,17 @@ def border?(x,y)
   end
 end
 
+puts "filename | \#cell coordinates (total) | \#border cells (total) | \#cells (total)"
 count = 0
 count_border = 0
+count_cells = 0
 time_start = Time.now
 files = Dir.entries(PATH)
 files = files.sort
 files.each do |file|
   if File.file?(PATH + '/' + file)
     $data = []
+    cells = Hash.new
     File.open(PATH + '/' + file) do |f|
       f.each do |line|
         $data << line.split(',').map(&:to_i)
@@ -36,11 +39,15 @@ files.each do |file|
       0.upto($cols-1) do |x|
         count += 1 if $data[y][x] > 0
         count_border += 1 if border?(x,y)
+        if cells[$data[y][x]].nil?
+          cells[$data[y][x]] = 1
+          count_cells += 1
+        end
       end
     end
-    puts "#{file} #{count} #{count_border}"
+    puts "#{file} #{count} #{count_border} #{count_cells}"
   end
 end
 puts (Time.now - time_start).to_s
 
-# refdataA_209.txt 46.246.907 12.243.993
+# refdataA_209.txt 46.246.907 12.243.993 244.789
