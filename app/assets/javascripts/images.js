e6470740c26ef754c3e3c1eeae8292b4b1b4acb5
Christian <wilde@informatik.uni-luebeck.de>
2013-01-10 12:35:31 +0100

    Visualization: Cell marking
    Added marking of single cells via mouse coordinates and minimum Euclidean distance to all cell centers. Furthermore, some refactoring of the existent visualization code.

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -17,49 +17,103 @@ function base64decode(data) {
   return result;
 }
 
-function displayCells(cells) {
-  var picture = $('#picture');
-  if (picture.length > 0) {
-    var canvas = $('<canvas>');
-    picture.after(canvas);
-    var p = picture.position();
-    canvas.css({
-      position: 'absolute',
-      top: p.top + 'px',
-      left: p.left + 'px',
-      width: picture.width() + 'px',
-      height: picture.height() + 'px'
-    });
-    
-    var c = canvas.get(0);
-    c.width = picture.width();
-    c.height = picture.height();
-    var context = c.getContext('2d');
-    context.clearRect(0, 0, picture.width(), picture.height());
-    
-    $.each(cells, function(index, cell) {
-      var mask = base64decode(cell.mask);
+function renderCell(cell, ctxCellmask, isMarked) {
+  // Decode cellmask.
+  var mask = base64decode(cell.mask);
+  
+  // Create ImageData object of proper size.
+  var image = ctxCellmask.getImageData(cell.left, cell.top, cell.width, 
+    cell.height);
 
-      // Create ImageData object of proper size).
-      var image = context.createImageData(cell.width, cell.height);
-      // TODO Implement color management.
-      for (var k = 0; k < cell.width * cell.height; k++) {
-        if (mask[k] > 0) {
-          image.data[4*k] = 96;
-          image.data[4*k+1] = 0;
-          image.data[4*k+2] = 0;
-          image.data[4*k+3] = 128;
-        }
+  // Update color values. If cell is marked, render blue else render red.
+  for (var k = 0; k < cell.width * cell.height; k++) {
+    if (mask[k] > 0) {
+      if (isMarked) {
+    	image.data[4*k]   = 0;
+    	image.data[4*k+2] = 96;
+      } else {
+    	image.data[4*k]   = 96;
+    	image.data[4*k+2] = 0;
       }
-
-      // Draw cellmask on cellmask canvas.
-      context.putImageData(image, cell.left, cell.top);
-    });
+      image.data[4*k+1] = 0;
+      image.data[4*k+3] = 128;
+    }
   }
+  
+  // Draw cellmask on cellmask canvas.
+  ctxCellmask.putImageData(image, cell.left, cell.top);
+}
+
+function getNearestCell(event, canvasCellmask, ctxCellmask) {
+  // Get x and y coordinates of mouse pointer on the canvas.
+  var x = 0;
+  var y = 0;
+  if (event.x == undefined && event.y == undefined) {	// Firefox
+    x = event.clientX;
+	y = event.clientY;
+  } else {												// all other browsers			
+    x = event.x;
+	y = event.y;
+  }
+  x -= canvasCellmask.offsetLeft - 1;
+  y -= canvasCellmask.offsetTop - 1;
+  
+  // Find nearest cell to that coordinates via minimum of Euclidean distances.
+  var minDistance = Infinity;
+  var idx = 0;
+  $.each(cells, function(index, cell) {
+    // Calculate Euclidean distance.	      
+    var distance = Math.sqrt(Math.pow(x - cell.center_x, 2) + 
+      Math.pow(y - cell.center_y, 2));
+
+    // Update minimum.
+	if (distance < minDistance) {
+	  minDistance = distance;
+	  idx = index;
+	}
+  });
+  	
+  if (idx != lastChosenCell) {
+    // Re-render last chosen cell in standard color.
+    renderCell(cells[lastChosenCell], ctxCellmask, false);  
+
+    // Render new cell in special color.
+    renderCell(cells[idx], ctxCellmask, true);  
+  }
+
+  // Update last chosen cell.
+  lastChosenCell = idx;
 }
 
 $(window).load(function () {
   if (cells) {
-    displayCells(cells);
+    // Initialize picture and canvas element.
+	var picture = $('#picture');
+	if (picture.length > 0) {
+	  var canvas = $('<canvas>',{'id':'cellmask'});
+	  picture.after(canvas);
+	  var p = picture.position();
+	  canvas.css({
+	    position: 'absolute',
+	    top: p.top + 'px',
+	    left: p.left + 'px',
+	    width: picture.width() + 'px',
+	    height: picture.height() + 'px'
+	  });
+	  var canvasCellmask = canvas.get(0);
+	  canvasCellmask.width = picture.width();
+	  canvasCellmask.height = picture.height();
+	  var ctxCellmask = canvasCellmask.getContext('2d');
+	  ctxCellmask.clearRect(0, 0, picture.width(), picture.height());
+	
+	  // Render all cell masks.
+	  $.each(cells, function(index, cell) { 
+		renderCell(cell, ctxCellmask, false);
+	  });
+    
+      // Add event listener on canvas element.
+      canvasCellmask.addEventListener("mousedown", function(event){
+    	getNearestCell(event, canvasCellmask, ctxCellmask)}, false);
+    }
   }
 });
